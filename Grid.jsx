0e75c8473f5a4e112d2a493dbea10cc954a394ab
SehrishAmbreen <ambareendesigns@gmail.com>
2025-02-01 20:36:51 +0500

    Add files via upload

---
+++ Grid.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import './grid.css'
+export default function Grid() {
+  return (
+    <div>
+        <div className="parent">
+      <div className="main">
+      <div className="grid">
+<div className="grid-items"><h1>A</h1></div>
+<div className="grid-items"><h1>B</h1></div>
+<div className="grid-items"><h1>C</h1></div>
+<div className="grid-items"><h1>D</h1></div>
+</div>
+<ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+      <div className="grid">
+<div className="grid-items"><h1>A</h1></div>
+<div className="grid-items"><h1>B</h1></div>
+<div className="grid-items"><h1>C</h1></div>
+<div className="grid-items"><h1>D</h1></div>
+</div>
+<ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+      <div className="grid">
+<div className="grid-items"><h1>A</h1></div>
+<div className="grid-items"><h1>B</h1></div>
+<div className="grid-items"><h1>C</h1></div>
+<div className="grid-items"><h1>D</h1></div>
+</div>
+<ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+      <div className="grid1">
+    <div className="grid-items"><h1>A</h1></div>
+    <div className="grid-items"><h1>B</h1></div>
+    <div className="grid-items"><h1>C </h1></div>
+    <div className="grid-items"><h1>D</h1></div>
+    <div className="grid-items"><h1>E</h1></div>
+</div>
+<ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+      <div className="grid1">
+    <div className="grid-items"><h1>A</h1></div>
+    <div className="grid-items"><h1>B</h1></div>
+    <div className="grid-items"><h1>C </h1></div>
+    <div className="grid-items"><h1>D</h1></div>
+    <div className="grid-items"><h1>E</h1></div>
+</div>
+<ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+        <div className="grid2">
+            <div className="grid-items"><h1>A</h1></div>
+            <div className="grid-items"><h1>B</h1></div>
+            <div className="grid-items"><h1>C</h1></div>
+            <div className="grid-items"><h1>D</h1></div>
+            <div className="grid-items"><h1>E</h1></div>
+        </div>
+        <ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+        <div className="grid3">
+            <div className="grid-item"></div>
+            <div className="grid-items"><h1>Header</h1></div>
+            <div className="grid-items"><h1>Sidebar</h1></div>
+            <div className="grid-items"><h1>Content</h1></div>
+        </div>
+        <ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+      <div className="main">
+        <div className="grid4">
+            <div className="grid-item"></div>
+            <div className="grid-items"><h1>Header</h1></div>
+            <div className="grid-items"><h1>Sidebar</h1></div>
+            <div className="grid-items"><h1>Content</h1></div>
+            <div className="grid-items"><h1>footer</h1></div>
+
+        </div>
+        <ins>Line-based placement spanning track</ins>
+<p>Positioning items which span more than one grid track</p>
+<a href="#">View example | Read specification</a>
+      </div>
+
+      </div>
+      
+    </div>
+  )
+}
